added posibility to output averaged bl in fxs average.py. Several bug fixes: polar vtk export without grid, ShCoeff, find connected component

xframe/externalLibraries/shtns_plugin.py
@@ -154,13 +154,13 @@ class ShSmall:
             return  np.array(tuple(synth_cplx(coeff) for coeff in data))
         inverse_cmplx = shape_change_decorator((self.n_coeff,),out_shape=self.angular_shape)(inverse_cmplx_inner)
         return inverse_cmplx
-    def get_empty_coeff(pre_shape=None):
+    def get_empty_coeff(self,pre_shape=None):
         if not isinstance(pre_shape,tuple):
             data = np.zeros(self.angular_shape,dtype=complex)
-            return ShCoff(data,self.ls,self.ms)
+            return ShCoeff(data,self.ls,self.ms)
         else:
             data = np.zeros(pre_shape+self.angular_shape,dtype=complex)
-            return ShCoff(data,self.ls,self.ms)
+            return ShCoeff(data,self.ls,self.ms)
     @property
     def grid(self):
         return GridFactory.construct_grid('uniform',(self.thetas,self.phis))

xframe/externalLibraries/vtk_plugin.py
@@ -5,6 +5,7 @@ from vtk.util import numpy_support as vtn
 from xframe.library.gridLibrary import GridFactory
 from xframe.library.gridLibrary import NestedArray
 from xframe.database.interfaces import VTKInterface
+from xframe.library.pythonLibrary import xprint
 log=logging.getLogger('root')
 
 class vtkSaver(VTKInterface):
@@ -41,9 +42,10 @@ class vtkSaver(VTKInterface):
                     break
             assert ~isinstance(writer,bool),'grid_type {} not known. Known grid_types are {}'.format(grid_type,vtkSaver.writer_dict.keys())
 
-            
+            #xprint(file_path)
             writer.SetFileName(file_path)
             writer.Update()
+            #xprint('saved')
         except AssertionError as e:
             log.error(e)
             
@@ -122,12 +124,11 @@ class vtkSaver(VTKInterface):
             new_data[:,0,-1] = data[:,0]
             datasets[n]=new_data
         if not (isinstance(grid,np.ndarray) or isinstance(grid,NestedArray)):
-            n_r,n_phi = datasets[0].shape
-            n_theta = 1
+            #xprint(f'dataset shape = {datasets[0].shape}')
+            n_r,_,n_phi = datasets[0].shape
             rs = np.arange(n_r)*1/n_r
             phis = np.arange(n_phi-1)*np.pi*2/(n_phi-1)
-            thetas = np.array([np.pi/2])
-            grid = GridFactory.construct_grid('uniform',[rs,thetas,phis])
+            grid = GridFactory.construct_grid('uniform',[rs,phis])
         phis = np.zeros(len(grid[0,:,1])+1,grid[0,:,1].dtype)
         phis[:-1]=grid[0,:,1]
         phis[-1] = 0 # periodic point
@@ -167,6 +168,7 @@ class vtkSaver(VTKInterface):
                 vtk_density.SetName("{}_{}".format(dset_names[n],n))
             else:
                 vtk_density.SetName("data_{}".format(n))
+        #xprint(vtk_grid)
         return vtk_grid
         
     @staticmethod