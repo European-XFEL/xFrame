ea2c1541f9060357ee1a458bdb23a567bb3dce03
Tim Berberich <tim.berberich@xfel.eu>
2026-08-07 11:12:08 +0200

    fix support dtype in averaging

--- xframe/projects/fxs/average.py
+++ xframe/projects/fxs/average.py
@@ -217,7 +217,9 @@ class DataLoader:
             self.current_h5_object = self.db.load(path,as_h5_object = True)
         o= self.current_h5_object
         result = o[f'reconstruction_results/{data_id}']
-        dataset = tuple((result['density_history'][-1],result['ft_density_history'][-1],result['support'][:]))
+        dataset = tuple((result['density_history'][-1].astype(complex),
+                         result['ft_density_history'][-1].astype(complex),
+                         result['support'][:].astype(complex)))
         return dataset
         
 
